b5ed94d8436e6f162d9c8362ddefb0e05584ef3b
ashwini-bochare11 <86307213+ashwini-bochare11@users.noreply.github.com>
2025-06-20 23:12:48 +0530

    Update PrescriptionRepository.java

--- app/src/main/java/com/project/back_end/repo/PrescriptionRepository.java
+++ app/src/main/java/com/project/back_end/repo/PrescriptionRepository.java
@@ -1,6 +1,6 @@
 package com.project.back_end.repo;
 
-import com.hospitalcms.entity.Prescription;
+import com.project.back_end.models.Prescription;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
