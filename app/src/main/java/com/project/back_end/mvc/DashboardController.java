e35b4bc7636231ca5a6bf18dad4989c735bfdbb5
ashwini-bochare11 <86307213+ashwini-bochare11@users.noreply.github.com>
2025-06-20 23:13:51 +0530

    Update DashboardController.java

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,11 +1,9 @@
 package com.project.back_end.mvc;
 
-package com.hospitalcms.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import com.hospitalcms.service.CommonService;
+import com.project.back_end.services.CommonService;
 
 @Controller
 public class DashboardController {
